85f40710261e6616c1ef64620e0d8741c19d534d
rafinder1 <rafpro33@gmail.com>
2024-04-27 20:21:37 +0200

    renamed name and return List<BanksModel>

--- lib/src/features/make_money_bank/data/remote_data_source/banks.dart
+++ lib/src/features/make_money_bank/data/remote_data_source/banks.dart
@@ -1,7 +1,8 @@
 import 'package:dio/dio.dart';
+import 'package:investment_keeper/src/features/make_money_bank/domain/models/banks_model.dart';
 
 class BanksRemoteDioDataSource {
-  Future<List<Map<String, dynamic>>?> getBanks() async {
+  Future<List<BanksModel>?> getBanks() async {
     final response = await Dio().get<List<dynamic>>(
         'https://my-json-server.typicode.com/rafinder1/ik-fake-backend/banks');
     final listDynamic = response.data;
@@ -9,7 +10,6 @@ class BanksRemoteDioDataSource {
     if (listDynamic == null) {
       return null;
     }
-
-    return listDynamic.map((e) => e as Map<String, dynamic>).toList();
+    return listDynamic.map((item) => BanksModel.fromJson(item)).toList();
   }
 }
