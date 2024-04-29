a34db5e6cdd520631393aa184f0d8cdb287088f5
rafinder1 <rafpro33@gmail.com>
2024-04-29 22:36:53 +0200

    Return empty list if listDynamic is null

--- lib/src/features/make_money_bank/data/remote_data_source/banks.dart
+++ lib/src/features/make_money_bank/data/remote_data_source/banks.dart
@@ -2,14 +2,17 @@ import 'package:dio/dio.dart';
 import 'package:investment_keeper/src/features/make_money_bank/domain/models/banks_model.dart';
 
 class BanksRemoteDioDataSource {
-  Future<List<BanksModel>?> getBanks() async {
+  Future<List<BanksModel>> getBanks() async {
     final response = await Dio().get<List<dynamic>>(
         'https://my-json-server.typicode.com/rafinder1/ik-fake-backend/banks');
     final listDynamic = response.data;
 
-    if (listDynamic == null) {
-      return null;
+    if (listDynamic != null) {
+      return listDynamic.map((item) => BanksModel.fromJson(item)).toList();
+    } else {
+      // Handle the case where listDynamic is null
+      // Return an empty list or throw an error based on your requirement
+      return [];
     }
-    return listDynamic.map((item) => BanksModel.fromJson(item)).toList();
   }
 }
