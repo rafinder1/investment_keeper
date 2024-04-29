114a3a6aabb2ef0306a1173a9bdc43a8d8f79c4f
rafinder1 <rafpro33@gmail.com>
2024-04-29 22:36:18 +0200

    Added method which download data in cubit

--- lib/src/features/make_money_bank/presentation/cubit/make_money_banks_cubit.dart
+++ lib/src/features/make_money_bank/presentation/cubit/make_money_banks_cubit.dart
@@ -1,9 +1,37 @@
 import 'package:bloc/bloc.dart';
 import 'package:investment_keeper/src/core/enums.dart';
+import 'package:investment_keeper/src/features/make_money_bank/data/remote_data_source/banks.dart';
 import 'package:investment_keeper/src/features/make_money_bank/domain/models/banks_model.dart';
 
 part 'make_money_banks_state.dart';
 
 class MakeMoneyBanksCubit extends Cubit<MakeMoneyBanksState> {
-  MakeMoneyBanksCubit() : super(MakeMoneyBanksState());
+  MakeMoneyBanksCubit(this.dataSource) : super(MakeMoneyBanksState());
+
+  final BanksRemoteDioDataSource dataSource;
+
+  Future<void> fetchData() async {
+    emit(
+      MakeMoneyBanksState(
+        status: Status.loading,
+      ),
+    );
+
+    try {
+      final results = await dataSource.getBanks();
+      emit(
+        MakeMoneyBanksState(
+          status: Status.success,
+          results: results,
+        ),
+      );
+    } catch (error) {
+      emit(
+        MakeMoneyBanksState(
+          status: Status.error,
+          errorMessage: error.toString(),
+        ),
+      );
+    }
+  }
 }
