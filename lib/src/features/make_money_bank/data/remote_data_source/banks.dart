82e479471bab995a68346871cc0d83e7c21c60a8
rafinder1 <rafpro33@gmail.com>
2024-04-30 21:32:38 +0200

    Translate json to BanksModel in repository

--- lib/src/features/make_money_bank/data/remote_data_source/banks.dart
+++ lib/src/features/make_money_bank/data/remote_data_source/banks.dart
@@ -1,18 +1,15 @@
 import 'package:dio/dio.dart';
-import 'package:investment_keeper/src/features/make_money_bank/domain/models/banks_model.dart';
 
 class BanksRemoteDioDataSource {
-  Future<List<BanksModel>> getBanks() async {
+  Future<List<Map<String, dynamic>>?> getBanks() async {
     final response = await Dio().get<List<dynamic>>(
         'https://my-json-server.typicode.com/rafinder1/ik-fake-backend/banks');
+
     final listDynamic = response.data;
 
-    if (listDynamic != null) {
-      return listDynamic.map((item) => BanksModel.fromJson(item)).toList();
-    } else {
-      // Handle the case where listDynamic is null
-      // Return an empty list or throw an error based on your requirement
-      return [];
+    if (listDynamic == null) {
+      return null;
     }
+    return listDynamic.map((e) => e as Map<String, dynamic>).toList();
   }
 }
