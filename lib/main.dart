7caa7b4a45c2fe9ec134f2ff17af0ada2cefdf36
rafinder1 <rafpro33@gmail.com>
2024-04-30 23:31:43 +0200

    delete bank page from main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:investment_keeper/src/core/enums.dart';
-import 'package:investment_keeper/src/features/make_money_bank/data/remote_data_source/banks.dart';
-import 'package:investment_keeper/src/features/make_money_bank/domain/repositories/banks_repository.dart';
-import 'package:investment_keeper/src/features/make_money_bank/presentation/cubit/make_money_banks_cubit.dart';
+import 'package:investment_keeper/src/features/make_money_bank/presentation/page/make_money_banks_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,55 +18,6 @@ class MyApp extends StatelessWidget {
       ),
       home: const Scaffold(
         body: BanksDataScreen(),
-      ),
-    );
-  }
-}
-
-class BanksDataScreen extends StatelessWidget {
-  const BanksDataScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: BlocProvider(
-        create: (context) => MakeMoneyBanksCubit(
-          BanksRepository(
-            banksRemoteDioDataSource: BanksRemoteDioDataSource(),
-          ),
-        )..fetchData(),
-        child: BlocBuilder<MakeMoneyBanksCubit, MakeMoneyBanksState>(
-          builder: (context, state) {
-            switch (state.status) {
-              case Status.initial:
-                return const Center(
-                  child: Text('Initial state'),
-                );
-              case Status.loading:
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              case Status.success:
-                return ListView.builder(
-                  itemCount: state.results.length,
-                  itemBuilder: (context, index) {
-                    final bank = state.results[index];
-                    return ListTile(
-                      title: Text(bank.name),
-                      subtitle: Text(
-                          'End Promotion: ${bank.endPromotion}, No Account Since: ${bank.noAccountSince}'),
-                    );
-                  },
-                );
-              case Status.error:
-                return Center(
-                  child: Text(
-                    state.errorMessage ?? 'Unknown error',
-                  ),
-                );
-            }
-          },
-        ),
       ),
     );
   }
