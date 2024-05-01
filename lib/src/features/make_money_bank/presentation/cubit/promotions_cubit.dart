ea59c6ab4493d296a2de4f64e013351ef54c2dba
rafinder1 <rafpro33@gmail.com>
2024-05-01 12:05:11 +0200

    Added Promotion Cubit

---
+++ lib/src/features/make_money_bank/presentation/cubit/promotions_cubit.dart
@@ -0,0 +1,37 @@
+import 'package:bloc/bloc.dart';
+import 'package:investment_keeper/src/core/enums.dart';
+import 'package:investment_keeper/src/features/make_money_bank/domain/models/promotion_model.dart';
+import 'package:investment_keeper/src/features/make_money_bank/domain/repositories/promotion_repository.dart';
+
+part 'promotions_state.dart';
+
+class PromotionsCubit extends Cubit<PromotionsState> {
+  PromotionsCubit(this.promotionRemoteRepository) : super(PromotionsState());
+
+  final PromotionRepository promotionRemoteRepository;
+
+  Future<void> fetchData() async {
+    emit(
+      PromotionsState(
+        status: Status.loading,
+      ),
+    );
+
+    try {
+      final results = await promotionRemoteRepository.getPromotionModel();
+      emit(
+        PromotionsState(
+          status: Status.success,
+          results: results,
+        ),
+      );
+    } catch (error) {
+      emit(
+        PromotionsState(
+          status: Status.error,
+          errorMessage: error.toString(),
+        ),
+      );
+    }
+  }
+}
