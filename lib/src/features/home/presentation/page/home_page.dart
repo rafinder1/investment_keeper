7f57e2508e1a7d9c5932dd43eb68bf2a7e3c9025
rafinder1 <rafpro33@gmail.com>
2024-06-13 20:57:41 +0200

    added home page

---
+++ lib/src/features/home/presentation/page/home_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Investment Keper'),
+      ),
+      body: const Center(
+        child: Text('Investment Keper'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.savings),
+            label: 'Investments',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.money_off),
+            label: 'Debts',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
